30a26033205b6482338ab2f7765d492674696564
AndresSg0716 <jandressg30@gmail.com>
2024-08-05 20:33:55 -0500

    .

--- 1-Agosto-(Ciclos-y-Scope)/Public/Js/index.js
+++ 1-Agosto-(Ciclos-y-Scope)/Public/Js/index.js
@@ -1,4 +1,3 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', () => {
     const inputFruta = document.getElementById('inputfruta');
     const addFrutaBtn = document.getElementById('addFrutaBtn');
